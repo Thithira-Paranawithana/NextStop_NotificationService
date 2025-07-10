59649e7279bde5bb2242671e53550956ca1b850c
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2025-07-10 15:03:39 +0530

    change comments

--- src/main/java/com/busticket/notificationservice/service/EmailSenderService.java
+++ src/main/java/com/busticket/notificationservice/service/EmailSenderService.java
@@ -1,29 +1,3 @@
-//package com.busticket.notificationservice.service;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class EmailSenderService {
-//
-//    private final JavaMailSender mailSender;
-//
-//    @Value("${spring.mail.from:quickbus.busservices@gmail.com}")
-//    private String from;
-//
-//    public void sendSimpleEmail(String to, String subject, String body) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom(from);
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(body);
-//        mailSender.send(message);
-//    }
-//}
 
 package com.busticket.notificationservice.service;
 
